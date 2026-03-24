cdc1d3b2328c1d28cda238bdc90c6fcb9e171f36
Palaksh Chaturvedi <palakshchaturvedi2022@vitbhopal.ac.in>
2026-03-24 12:12:01 +0530

    Modify project entries in ProjectSection.jsx
    Updated project IDs and added a new project entry.

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -3,6 +3,15 @@ import { ArrowRight, ExternalLink, Github } from "lucide-react"
 const projects = [
     {
         id: 1,
+        title: "mepass",
+        description: "An event ticket booking system.",
+        image: "/projects/project6.png",
+        tags: ["MERN", "TailwindCSS", "MongoDB"],
+        demoUrl: "https://mepass-frontend.onrender.com",
+        githubUrl: "https://github.com/Palakshchat15/mepass"
+    },
+    {
+        id: 2,
         title: "Interview Prep AI",
         description: "A platform to prepare for interview through AI.",
         image: "/projects/project5.png",
@@ -11,7 +20,7 @@ const projects = [
         githubUrl: "https://github.com/Palakshchat15/interviewprep"
     },
     {
-        id: 2,
+        id: 3,
         title: "Prescripto User Page",
         description: "A User Page for a Doctor Appointment Booking System.",
         image: "/projects/project1.png",
@@ -20,7 +29,7 @@ const projects = [
         githubUrl: "https://github.com/Palakshchat15/prescripto"
     },
     {
-        id: 3,
+        id: 4,
         title: "Prescripto Admin Page",
         description: "An Admin Page for a Doctor Appointment Booking System.",
         image: "/projects/project2.png",
@@ -29,7 +38,7 @@ const projects = [
         githubUrl: "https://github.com/Palakshchat15/prescripto"
     },
     {
-        id: 4,
+        id: 5,
         title: "Snapgram",
         description: "A Social Media App.",
         image: "/projects/project3.png",
@@ -38,7 +47,7 @@ const projects = [
         githubUrl: "https://github.com/Palakshchat15/snapgram"
     },
     {
-        id: 5,
+        id: 6,
         title: "NewVideos",
         description: "A Video Streaming App.",
         image: "/projects/project4.png",
